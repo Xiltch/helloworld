2dbb4ab95df266cb52f972c51851c7c0a849b454
Jonathan Tweedle <Jonathan Tweedle>
2017-03-09 19:04:30 -0800

    Got the app working in both electron and the lite server

--- src/app/components/hello.component.ts
+++ src/app/components/hello.component.ts
@@ -1,17 +1,49 @@
 import { Component } from '@angular/core';
 
+export class Item {
+    id: number;
+    title: string;
+}
+
+const ITEMS: Item[] = [
+    { id: 1, title: 'First Item'},
+    { id: 2, title: 'Second Item'},
+    { id: 3, title: 'Third Item'}
+];
+
 @Component({
     selector: 'hw-main',
     template: `
     <div>
       {{ text }}
+      <br>
+      <h2>{{item.title}}</h2>
+      <div>
+        <label>title: </label>
+        <br>
+        <input [(ngModel)]="item.title" placeholder="title">
+      </div>
+      <div>
+        <ul>
+            <li *ngFor="let x of items">
+                {{x.id}}: {{x.title}}
+            </li>
+        </ul>
+      </div>
     </div>
   `
 })
-
 export class HelloComponent {
     public text: string;
+    item: Item;
+    items: Item[];
+
     constructor() {
         this.text = 'Hello Component has loaded!';
+        this.item = {
+            id: 0,
+            title: 'Dummy Item'
+        };
+        this.items = ITEMS;
     }
 }
--- src/app/module.ts
+++ src/app/module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
 import { HelloComponent } from './components/hello.component';
 
 
 @NgModule({
-    imports: [BrowserModule],
+    imports: [BrowserModule, FormsModule],
     declarations: [HelloComponent],
     bootstrap: [HelloComponent]
 })
-
 export class AppModule { }
